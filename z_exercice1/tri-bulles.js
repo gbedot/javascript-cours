5ed2bfc8c34958205cf5f31bea630de793926aff
gbedot <gb.guillaume.bedot@gmail.com>
2021-11-23 21:21:06 +0100

    exercice 1

---
+++ z_exercice1/tri-bulles.js
@@ -0,0 +1,24 @@
+function tribulles (array) {
+ 
+let valeur;
+
+let permut = 1;
+
+
+do {
+    for (let i = 0; i < array.length-1; i++) {
+        if (array[i]>array[i+1])
+        {
+            valeur = array[i];
+            array[i] = array[i+1];
+            array[i+1] = valeur;
+            permut = 1;
+        }
+    }
+    console.log(permut)
+} while (permut=1)
+return array;
+
+}
+
+console.log(tribulles([1,7,158,6,7896,12,78]));
